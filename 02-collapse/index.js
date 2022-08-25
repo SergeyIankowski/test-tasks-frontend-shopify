fc9f9c02b9c7727351eb6b50368d3b786d9530c4
SeregeyIankowski <garret933@gmail.com>
2022-08-25 12:05:19 +0300

    feat: add collaps functionality to button in task2

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,21 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const COLLAPSIBLE_BUTTON = document.querySelector('.collapsible__button');
+const COLLAPSIBLE_CONTENT = document.querySelector('.collapsible__content');
+const COLLAPSIBLE_ACTONS = document.querySelectorAll('.collapsible__action');
+
+const changeButtonValue = (show) => {
+  COLLAPSIBLE_ACTONS[0].hidden = show;
+  COLLAPSIBLE_ACTONS[1].hidden = !show;
+}
+changeButtonValue(true);
+
+const showContent = (showContent) => {
+  COLLAPSIBLE_CONTENT.hidden = !showContent;
+}
+showContent(false);
+
+let isShow = false;
+COLLAPSIBLE_BUTTON.addEventListener('click', () => {
+  isShow ? changeButtonValue(true) : changeButtonValue(false);
+  isShow ? showContent(false) : showContent(true);
+  isShow = !isShow;
+})
